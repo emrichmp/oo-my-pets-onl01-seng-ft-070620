c718a90c3835c81f8810152c99e858458f27e044
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-28 00:05:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,45 @@
 class Owner
-  # code goes here
+  attr_accessor :pets, :cats, :dogs
+  attr_reader :name, :species
+  @@all = []
+  
+  
+  def initialize(name=nil)
+    @name = name
+    @species = species
+    @@all << self
+    @pets = {:cats => [], :dogs => []}
+  end
+  
+  def species()
+    @species = "human"
+  end
+  
+  def say_species()
+    "I am a #{@species}."
+  end
+  
+  def self.all()
+    @@all
+  end
+  
+  def self.count()
+    @@all.length
+  end
+  
+  def self.reset_all()
+    @@all.clear
+  end
+  
+  def buy_cat(cat_name)
+    @pets[:cats] << Cat.new(cat_name, self)
+  end
+
+  def buy_dog(dog_name)
+    @pets[:dogs] << Dog.new(dog_name, self)
+  end
+  
+  def cats()
+    @pets[:cats]
+  end
 end
